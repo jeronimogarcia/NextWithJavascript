9ca15c2ac0550d3ecca454e69ddb989ff68300ba
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2022-12-20 14:25:12 +0100

    Active Links

--- components/ActiveLink.js
+++ components/ActiveLink.js
@@ -1,10 +1,20 @@
-import Link from 'next/link'
-import React from 'react'
+import React from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
-const ActiveLink = ({text, href}) => {
+const style = {
+  color: "#0070f3",
+  textDecoration: "underline",
+};
+
+const ActiveLink = ({ text, href }) => {
+  const { asPath } = useRouter();
+
   return (
-    <Link href={href}>{text}</Link>
-  )
-}
+    <Link href={href} style={asPath === href ? style : null}>
+      {text}
+    </Link>
+  );
+};
 
-export default ActiveLink
+export default ActiveLink;
